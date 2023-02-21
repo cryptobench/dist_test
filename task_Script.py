14c9ab82f9c4c92f39c23641c36a12b7d412dee3
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2023-02-21 10:22:19 +0100

    Update task_Script.py

--- task_Script.py
+++ task_Script.py
@@ -10,23 +10,27 @@ import sys
 # examples_dir = pathlib.Path(__file__).resolve().parent.parent
 # sys.path.append(str(examples_dir))
 
+
 async def worker(ctx: WorkContext, tasks: AsyncIterable[Task]):
     script = ctx.new_script(timeout=timedelta(minutes=10))
-    script.upload_file('USA_Housing.csv','/Golem/input/USA_Housing.csv')
-    
+    # script.upload_file('USA_Housing.csv', '/Golem/input/USA_Housing.csv')
+
     async for task in tasks:
-        future_result = script.run("/bin/bash", "-c", "chmod 777 /code/run-model.sh && chmod +x /code/run-model.sh")
-        # future_result = script.run("/code/run-model.sh")
-        # future_result = script.run("/bin/bash", "-c", "/code/run-model.sh")
-        script.download_file(f"/bin/bash/Golem/output/mse_data.csv", "output/mse_data.csv")
-        script.download_file(f"/bin/bash/Golem/output/coefficient_data.csv", "output/coefficient_data.csv")
+        future_result = script.run(
+            "/bin/bash", "-c", "chmod 777 /code/run-model.sh && chmod +x /code/run-model.sh")
+        future_result = script.run(
+            "/bin/bash", "-c", "/usr/bin/python /code/Golem.py")
+        script.download_file(
+            f"/Golem/output/mse_data.csv", "output/mse_data.csv")
+        # script.download_file(
+        #     f"/dist_test/output/coefficient_data.csv", "output/coefficient_data.csv")
         yield script
         task.accept_result(result=await future_result)
 
 
 async def main():
     package = await vm.repo(
-        image_hash="b1dbf1dda4d3c9628d8cd144b64c8ea7c6793ccaf6fb5f58d64a373a",
+        image_hash="3bd43146e2b614106745a70e04cbffa0462fa66e5dee0c87f4171554",
     )
 
     tasks = [Task(data=None)]
@@ -43,11 +47,10 @@ if __name__ == "__main__":
     task = loop.create_task(main())
     loop.run_until_complete(task)
 
-#How is the data being passed on the upload and download file. What is the mechanism.
-#What happens if I lose my internet conections? 
-#Can people see what is on my image hash? 
-#Can other people run on my image hash? Is it private only to me?
-#Is it encrypted? If so how is the other party decrypting it?
+# How is the data being passed on the upload and download file. What is the mechanism.
+# What happens if I lose my internet conections?
+# Can people see what is on my image hash?
+# Can other people run on my image hash? Is it private only to me?
+# Is it encrypted? If so how is the other party decrypting it?
 
-#How does the consensur mechanisum work?
-
+# How does the consensur mechanisum work?
